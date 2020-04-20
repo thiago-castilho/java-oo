b2246d75773be25d8476e0ba267982aaebe1a613
Thiago <tcosta1996@hotmail.com>
2020-04-19 23:56:02 -0300

    Add Capitulo 7 - Interface

--- livraria/src/livraria/Ebook.java
+++ livraria/src/livraria/Ebook.java
@@ -1,6 +1,6 @@
 package livraria;
 
-public class Ebook extends Livro {
+public class Ebook extends Livro implements Promocional {
 
     private String waterMark;
 
--- livraria/src/livraria/LivroFisico.java
+++ livraria/src/livraria/LivroFisico.java
@@ -1,6 +1,6 @@
 package livraria;
 
-public class LivroFisico extends Livro {
+public class LivroFisico extends Livro implements Promocional {
 
     public LivroFisico(Autor autor) {
         super(autor);
