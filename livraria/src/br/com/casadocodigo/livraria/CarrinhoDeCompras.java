1f697764159cfe4a42a2a652fe99d2cedae8e8a1
Thiago <tcosta1996@hotmail.com>
2020-04-28 23:56:57 -0300

    Add Capitulo 9 - Arrays e Exceptions

--- livraria/src/br/com/casadocodigo/livraria/CarrinhoDeCompras.java
+++ livraria/src/br/com/casadocodigo/livraria/CarrinhoDeCompras.java
@@ -5,13 +5,23 @@ import br.com.casadocodigo.livraria.produtos.Produto;
 public class CarrinhoDeCompras {
 
     private double total;
+    private Produto[] produto =  new Produto[10];
+    private int contador = 0;
 
     public void adiciona(Produto produto) {
         System.out.println("Adicionando: " + produto);
+        this.produto[contador] = produto;
+        contador++;
         this.total += produto.getValor();
     }
 
     public double getTotal() {
         return this.total;
     }
+
+    public Produto[] getProdutos() {
+        return this.produto;
+    }
+
+
 }
--- livraria/src/br/com/casadocodigo/livraria/teste/RegistroDeVendas.java
+++ livraria/src/br/com/casadocodigo/livraria/teste/RegistroDeVendas.java
@@ -4,6 +4,7 @@ import br.com.casadocodigo.livraria.Autor;
 import br.com.casadocodigo.livraria.CarrinhoDeCompras;
 import br.com.casadocodigo.livraria.produtos.Ebook;
 import br.com.casadocodigo.livraria.produtos.LivroFisico;
+import br.com.casadocodigo.livraria.produtos.Produto;
 
 public class RegistroDeVendas {
 
@@ -15,10 +16,9 @@ public class RegistroDeVendas {
         fisico.setNome("Test Driven Development");
         fisico.setValor(59.90);
 
-        Ebook ebook = new Ebook(autor);
+        Ebook ebook = new Ebook(null);
         ebook.setNome("Test Driven Development");
         ebook.setValor(29.90);
-
 
         if (fisico.aplicaDescontoDe10Porcento()) {
             System.out.println("Valor agora é: " + fisico.getValor());
@@ -28,5 +28,24 @@ public class RegistroDeVendas {
         carrinho.adiciona(ebook);
 
         System.out.println("Total: " + carrinho.getTotal());
+
+        Produto[] produtos = carrinho.getProdutos();
+
+        // for(Produto produto : produtos) {
+        for (int i = 0; i <= produtos.length; i++) {
+            try {
+                Produto produto = produtos[i];
+                if (produto != null) {
+                    System.out.println("Total: " + produto.getValor());
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Deu exception no indice:  " + i);
+            } catch (NullPointerException e) {
+                System.out.println("Array não foi instanciado!");
+            }
+        }
+
+        System.out.println("Fui executado!");
+
     }
 }
